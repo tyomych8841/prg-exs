350dd661a640b80c86a66deb9d95827f6501a263
tyomych8841 <ekmakarek9576@vk.com>
2022-03-30 18:16:55 +0700

    8

---
+++ 8/src/Main.java
@@ -0,0 +1,20 @@
+public class Main{
+	public static void main(String[] args){
+		byte b = 127;
+		short s = 32767;
+		int i = 2147483647;
+		long l = 922337203685577580l;
+		float f = 1.11111f;
+		double d = 9.99999999999;
+		char c = 'a';
+		boolean bool = true;
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(c+" "+bool);
+	}
+}
+
